4e5efe4def8ef019b55181dc25e6cad192216add
Anthony Timoshin <95761331+anthonytimoshin@users.noreply.github.com>
2024-05-19 17:59:26 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,16 +5,28 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.bind(("127.0.0.1", 65000))  # Привязываем серверный сокет к localhost и 65000 порту.
 s.listen(1)  # Начинаем прослушивать входящие соединения.
-conn, addr = s.accept()  # Метод который принимает входящее соединение.
 
 while True:
+    conn, addr = s.accept()  # Метод, который принимает входящее соединение.
+    start = time.time()
+
     data = conn.recv(1024)  # Получаем данные из сокета.
+
+    if (time.time() - start) >= 30.0:  # Отключаем клиента от сервера через 30 секунд задержки
+        data = "Server timed out, connection closed. Try again. Server was written by Timoshin A.A. M3O-107B-23 \0"
+        conn.sendall(data.encode('UTF-8'))
+        conn.close()
+        print("Connection closed.")
+
     if not data:
+        current_time = time.time()
         break
-    data = data[::-1]
-    message = " Сервер написал Тимошин А.А. М3О-107Б-23"
-    data = data + message.encode('utf-8')
-    time.sleep(1.0)  # имитация работы сервера (задержка в 1 секунду)
-    conn.sendall(data)  # Отправляем данные в сокет.
+
+    data = data.decode('UTF-8')[::-1]  # Разворачиваем сообщение клиента
+    message = " Сервер написан Тимошиным А.А. М3О-107Б-23 \0"
+    data = data + message
+
+    time.sleep(5.0)  # Имитация работы сервера (задержка в 5 секунд)
+    conn.sendall(data.encode('UTF-8'))  # Отправляем данные в сокет.
 
 conn.close()
